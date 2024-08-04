d96374f0527ccfd3ac0b3a59906df880b2a806bf
Jordi Duran <jordiduranpesantez@gmail.com>
2024-08-04 11:13:32 -0500

    feat: added Banco text into header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ImagesConfig } from '../../../core/images-config/images-config';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent {
+  ImagesConfig = ImagesConfig;
 
+  logo = ImagesConfig.logo;
 }
