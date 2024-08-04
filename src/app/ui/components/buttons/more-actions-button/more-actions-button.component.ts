50d9e0211fbac3bbfb975562df973d6de0e53e97
Jordi Duran <jordiduranpesantez@gmail.com>
2024-08-04 15:17:10 -0500

    feat: added options to more ver options component

--- src/app/ui/components/buttons/more-actions-button/more-actions-button.component.ts
+++ src/app/ui/components/buttons/more-actions-button/more-actions-button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-more-actions-button',
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
   templateUrl: './more-actions-button.component.html',
   styleUrl: './more-actions-button.component.scss',
 })
-export class UiMoreActionsButtonComponent {}
+export class UiMoreActionsButtonComponent {
+  @Output() edit = new EventEmitter<void>();
+  @Output() delete = new EventEmitter<void>();
+
+  editItem() {
+    this.edit.emit();
+  }
+
+  deleteItem() {
+    this.delete.emit();
+  }
+}
